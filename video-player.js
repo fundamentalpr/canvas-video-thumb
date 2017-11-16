d70e12b0af4d8a2a9451a60c3a2a994d6168abf1
Mark Van de Velde <33706374+mrdatacommando@users.noreply.github.com>
2017-11-17 10:56:47 +1100

    Update and rename video-player-full-v1.2.js to video-player.js
    Now loads image across browsers
    Working on PC for: IE11, FF52, Chrome62

--- video-player.js
+++ video-player.js
@@ -1,10 +1,6 @@
-//----- canvas video media thumbnail enlager script -----------------
-//-------------------------------------------------------------------
+//----- Canvas video media thumbnail enlager script -----------------
 //----- By: Mark Van de Velde ---------------------------------------
-//----- Thanks to James Jones for help with the MutationObserver ----
-//----- For installation as Javascript in Canvas Theme editor -------
-//----- Provided with no warranty! - test as required ---------------
-//----- V1.2--11|2017 -----------------------------------------------
+//----- V1.3--11|2017 -----------------------------------------------
 
 (function(){
   //identify lowest level item before expected changes
@@ -12,7 +8,7 @@
   if (el){
     //use new video player item for observer check
     var check = el.querySelectorAll('span.media_comment_thumbnail_play_button');
-   	//locate main media eliment by class name [instructure_inline_media_comment]
+   	//locate main media element by class name [instructure_inline_media_comment]
 	var mcID = document.getElementsByClassName('instructure_inline_media_comment');
     if (check.length>0){
       	// Canvas beat us to it, there's no need for an observer
@@ -40,8 +36,12 @@
   		//locate the the videoplayer by selecting the medias first child (a span tag), 
 		//update the background image from the media server using the id from the [media_comment_][m-xxxxx]
 		//NOTE: the media server can be located using the Global JS variable: INST.kalturaSettings.resource_domain
-		//---- in this case its: apse2.nv.instructuremedia.com
-		mcID[i].firstChild.style='background-image:url("https://apse2.nv.instructuremedia.com/p/2/thumbnail/entry_id/'+mcID[i].id.split('media_comment_')[1]+'/width/540/height/320/bgcolor/000000/type/2/vid_sec/5");height:320px;width:540px;';		
+		//in this case its: apse2.nv.instructuremedia.com		
+		//image dimensions are determined by the original upload video, however the URL is always the same including: /width/540/height/320/.
+		//having a set dimension ratio will provide a consistent player appearance, smaller videos may appear stretched. 
+		mcID[i].firstChild.style.width='640px';
+		mcID[i].firstChild.style.height='360px';
+		mcID[i].firstChild.style.background='url("https://apse2.nv.instructuremedia.com/p/2/thumbnail/entry_id/'+mcID[i].id.split('media_comment_')[1]+'/width/540/height/320/bgcolor/000000/type/2/vid_sec/5")';	
   	}
   }
 })();
